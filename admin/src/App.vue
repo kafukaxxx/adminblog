cdfcd93dc85e69bf0b0c241a7145665195efbdce
jj18 <dgg86net@outlook.com>
2023-07-16 20:34:55 +0800

    router

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <a-config-provider>
+    <div id="app">
+      <router-view>
+
+      </router-view>
+    </div>
+  </a-config-provider>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import { zhCN } from "ant-design-vue/lib/locale-provider/zh_CN";
+export default {
+  data() {
+    return {
+      locale:zhCN
+    }
+  }
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
